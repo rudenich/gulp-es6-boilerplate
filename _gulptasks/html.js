f31e442fefde6518e42062f5f1d906847850aac4
Rudenich <rudenich@gmail.com>
2017-08-09 09:44:34 +0300

    twig json data source

--- _gulptasks/html.js
+++ _gulptasks/html.js
@@ -2,6 +2,8 @@ import paths  from '../gulp.paths.json';
 import gulp from 'gulp';
 import twig from 'gulp-twig';
 
+import fs from "fs";
+
 import browserSync from "browser-sync";
 
 export default ()=> {
@@ -9,9 +11,12 @@ export default ()=> {
         .pipe(gulp.dest(paths.build.html))
     ;
 
+    let data = JSON.parse(fs.readFileSync('src/data/content.json'));
+    
     return gulp.src(paths.src.html + '/*.twig')
         .pipe(twig({
-            base: paths.src.html
+            base: paths.src.html,
+            data: data
         }))
         .on('error', (message)=> {
             console.log(message);
